dd2082fa1e2c0e403437c6f58ba8478842897d2d
aTc <aTc@k-n-p.org>
2015-04-26 20:30:32 +0200

    multistrap: replace all testing with jessie

--- multistrap/build.sh
+++ multistrap/build.sh
@@ -3,4 +3,4 @@ multistrap -f pyra-xfce.conf
 # Move image into image-dir
 echo Moving image into the public directory...
 NOW=$(date +"%Y-%m-%d_%H-%M")
-mv ./pyra-debian-jessie-xfce-rootfs.tgz /srv/www/vhosts/pyra-handheld.com/domains/packages.pyra-handheld.com/httpdocs/rootfs/pyra-jessie-testing-xfce4-rootfs-$NOW.tgz
+mv ./pyra-debian-jessie-xfce-rootfs.tgz /srv/www/vhosts/pyra-handheld.com/domains/packages.pyra-handheld.com/httpdocs/rootfs/pyra-debian-jessie-xfce-rootfs-$NOW.tgz
